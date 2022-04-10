bc5ff50cb08c3116e408219a6343adfa92cbdc22
Matthew De Leon <mattjdeleon@gmail.com>
2022-04-09 23:14:49 -0600

    background plus wider textbox

--- testEvent.js
+++ testEvent.js
@@ -3,6 +3,6 @@ var input = document.getElementById("input");
 input.addEventListener("keydown", function (e) {
     var keyname = e.key;
     var outputMessage = "".concat(keyname, " key is pressed");
-    output.style.color = "green";
+    output.style.color = "blue";
     output.innerText = outputMessage;
 });
--- testEvent.ts
+++
@@ -1,9 +0,0 @@
-const output = document.getElementById("output-message");
-const input = document.getElementById("input");
-
-input.addEventListener("keydown", (e) => {
-  const keyname = e.key;
-  const outputMessage = `${keyname} key is pressed`;
-  output.style.color = "green";
-  output.innerText = outputMessage;
-});
